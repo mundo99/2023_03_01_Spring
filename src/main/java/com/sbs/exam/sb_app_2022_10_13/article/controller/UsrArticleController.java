c938a3c8d7504537400f8590e2f85b27c14a521b
mundo99 <ansehdus7@naver.com>
2023-03-01 23:48:55 +0900

    테스트용 게시물 10개 자동생성

--- src/main/java/com/sbs/exam/sb_app_2022_10_13/article/controller/UsrArticleController.java
+++ src/main/java/com/sbs/exam/sb_app_2022_10_13/article/controller/UsrArticleController.java
@@ -20,6 +20,19 @@ public class UsrArticleController {
     public UsrArticleController(){
         articles = new ArrayList<>();
         articleLastId = 0;
+
+        makeTestData();
+    }
+    private void makeTestData(){
+        for(int i = 1; i <= 10; i++){
+            int id = articleLastId + 1;
+            String title = "제목" + i;
+            String body = "내용" + i;
+            Article article = new Article(id, title, body);
+
+            articles.add(article);
+            articleLastId = id;
+        }
     }
 
     @RequestMapping("/user/article/doAdd")
