946df40a27615dc96b024419f59c68099fae6e1c
mundo99 <ansehdus7@naver.com>
2023-03-01 21:58:23 +0900

    호출 시 반환되는 값이 1씩 커지는 컨트롤러 구현

---
+++ src/main/java/com/sbs/exam/sb_app_2022_10_13/home/controller/UsrHomeController.java
@@ -0,0 +1,39 @@
+package com.sbs.exam.sb_app_2022_10_13.home.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class UsrHomeController {
+    private int count;
+
+    public UsrHomeController() {
+        count = -1;
+    }
+
+    @RequestMapping("/user/home/main")
+    @ResponseBody
+    public String showMain(){
+        return "안녕하세요.ㅎㅎ";
+    }
+
+    @RequestMapping("/user/home/main2")
+    @ResponseBody
+    public String showMain2(){
+        return "반갑습니다.ㅎㅎ";
+    }
+
+    @RequestMapping("/user/home/main3")
+    @ResponseBody
+    public String showMain3(){
+        return "또 만나요.ㅎㅎ";
+    }
+
+    @RequestMapping("/user/home/main4")
+    @ResponseBody
+    public int showMain4(){
+        count++;
+        return count;
+    }
+}
