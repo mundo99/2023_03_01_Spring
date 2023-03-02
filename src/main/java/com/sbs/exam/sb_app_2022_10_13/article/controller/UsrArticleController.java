7648caa2fe98cb946306c206cd8592592d41b5f7
mundo99 <ansehdus7@naver.com>
2023-03-02 13:35:51 +0900

    DB접속 정보 수정

--- src/main/java/com/sbs/exam/sb_app_2022_10_13/article/controller/UsrArticleController.java
+++ src/main/java/com/sbs/exam/sb_app_2022_10_13/article/controller/UsrArticleController.java
@@ -1,6 +1,8 @@
 package com.sbs.exam.sb_app_2022_10_13.article.controller;
 
+import com.sbs.exam.sb_app_2022_10_13.article.service.ArticleService;
 import com.sbs.exam.sb_app_2022_10_13.article.vo.Article;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -13,86 +15,41 @@ import java.util.Map;
 @Controller
 public class UsrArticleController {
 
-    int articleLastId;
-    private List<Article> articles;
+    @Autowired
+    private ArticleService articleService;
 
-    public UsrArticleController(){
-        articles = new ArrayList<>();
-        articleLastId = 0;
-
-        makeTestData();
-    }
-    private void makeTestData(){
-        for(int i = 1; i <= 10; i++){
-            String title = "제목" + i;
-            String body = "내용" + i;
-
-            witeArticle(title,body);
-        }
-    }
-
-
-    private Article witeArticle(String title, String body) {
-        int id = articleLastId + 1;
-        Article article = new Article(id, title, body);
-        articles.add(article);
-        articleLastId = id;
-
-        return article;
-    }
-
-    private void deleteArticle(int id) {
-        Article article = getArticle(id);
-        articles.remove(article);
-    }
-
-    private void ModifyArticle(int id, String title,String body) {
-        Article article = getArticle(id);
-
-        article.setTitle(title);
-        article.setBody(body);
-    }
-
-    private Article getArticle(int id) {
-        for ( Article article : articles ){
-            if(article.getId() == id){
-                return article;
-            }
-        }
-        return null;
-    }
 
     @RequestMapping("/user/article/doAdd")
     @ResponseBody
     public Article doAdd(String title, String body){
 
-        Article article = witeArticle(title, body);
+        Article article = articleService.writeArticle(title, body);
 
         return article;
     }
     @RequestMapping("/user/article/getArticles")
     @ResponseBody
     public List<Article> getArticles(){
-        return articles;
+        return articleService.getArticles();
     }
 
     @RequestMapping("/user/article/doDelete")
     @ResponseBody
     public String doDelete(int id){
-        Article article = getArticle(id);
+        Article article = articleService.getArticle(id);
 
         if( article == null ){
             return id + "번 게시물이 존재하지 않습니다.";
         }
 
-        deleteArticle(id);
+        articleService.deleteArticle(id);
         return id + "번 게시물을 삭제하였습니다.";
     }
 
     @RequestMapping("/user/article/getArticle")
     @ResponseBody
     public Object getArticleAction(int id){
-        Article article = getArticle(id);
+        Article article = articleService.getArticle(id);
 
         if( article == null ){
             return id + "번 게시물이 존재하지 않습니다.";
@@ -104,13 +61,13 @@ public class UsrArticleController {
     @RequestMapping("/user/article/doModify")
     @ResponseBody
     public String doModify(int id, String title, String body){
-        Article article = getArticle(id);
+        Article article = articleService.getArticle(id);
 
         if( article == null ){
             return id + "번 게시물이 존재하지 않습니다.";
         }
 
-        ModifyArticle(id, title, body);
+        articleService.ModifyArticle(id, title, body);
         return id + "번 게시물을 수정하였습니다.";
 
     }
--- src/main/java/com/sbs/exam/sb_app_2022_10_13/article/service/ArticleService.java
+++ src/main/java/com/sbs/exam/sb_app_2022_10_13/article/service/ArticleService.java
@@ -1,8 +1,42 @@
 package com.sbs.exam.sb_app_2022_10_13.article.service;
 
+import com.sbs.exam.sb_app_2022_10_13.article.controller.UsrArticleController;
+import com.sbs.exam.sb_app_2022_10_13.article.repository.ArticleRepository;
+import com.sbs.exam.sb_app_2022_10_13.article.vo.Article;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class ArticleService {
+    @Autowired
+    private ArticleRepository articleRepository;
 
+    public ArticleService(ArticleRepository articleRepository){
+        this.articleRepository = articleRepository;
+        articleRepository.makeTestData();
+    }
+
+    public Article writeArticle(String title, String body) {
+        return articleRepository.writeArticle(title, body);
+    }
+
+    public List<Article> getArticles() {
+        return articleRepository.getArticles();
+    }
+
+    public Article getArticle(int id) {
+        return articleRepository.getArticle(id);
+    }
+
+    public void deleteArticle(int id) {
+        articleRepository.deleteArticle(id);
+    }
+
+    public void ModifyArticle(int id, String title, String body) {
+        articleRepository.ModifyArticle(id, title, body);
+    }
 }
+
