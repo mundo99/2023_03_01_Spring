97e7d1f8e943dab895012597cfee96a9accad49b
mundo99 <ansehds7@gmail.com>
2023-03-13 11:33:25 +0900

    "ut.empty 함수 도입"

--- src/main/java/com/sbs/exam/sb_app_2022_10_13/membver/controller/UsrMemberController.java
+++ src/main/java/com/sbs/exam/sb_app_2022_10_13/membver/controller/UsrMemberController.java
@@ -3,6 +3,7 @@ package com.sbs.exam.sb_app_2022_10_13.membver.controller;
 import com.sbs.exam.sb_app_2022_10_13.article.vo.Article;
 import com.sbs.exam.sb_app_2022_10_13.membver.service.MemberService;
 import com.sbs.exam.sb_app_2022_10_13.membver.vo.Member;
+import com.sbs.exam.sb_app_2022_10_13.utill.Ut;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,29 +18,29 @@ public class UsrMemberController {
     @Autowired
     private MemberService memberService;
 
-//
+
     @RequestMapping("/usr/member/doJoin")
     @ResponseBody
     public Object doJoin(String loginId, String loginPw, String name, String nickname,
                          String cellphoneNo, String email){
         int id = memberService.join( loginId,  loginPw,  name,   nickname, cellphoneNo,  email);
 
-        if(loginId == null) {
+        if(Ut.empty(loginId)) {
             return "loginId(을)를 입력해주세요";
         }
-        if(loginPw == null) {
+        if(Ut.empty(loginPw)) {
             return "loginPw(을)를 입력해주세요";
         }
-        if(name == null) {
+        if(Ut.empty(name)) {
             return "name(을)를 입력해주세요";
         }
-        if(nickname == null) {
+        if(Ut.empty(nickname)) {
             return "nickname(을)를 입력해주세요";
         }
-        if(cellphoneNo == null) {
+        if(Ut.empty(cellphoneNo)) {
             return "cellphoneNo(을)를 입력해주세요";
         }
-        if(email == null) {
+        if(Ut.empty(email)) {
             return "email(을)를 입력해주세요";
         }
         if( id == -1){
---
+++ src/main/java/com/sbs/exam/sb_app_2022_10_13/utill/Ut.java
@@ -0,0 +1,15 @@
+package com.sbs.exam.sb_app_2022_10_13.utill;
+
+public class Ut {
+    public static boolean empty(Object obj) {
+        if(obj == null){
+            return true;
+        }
+        if( obj instanceof String == false) {
+            return true;
+        }
+        String str = (String) obj;
+
+        return str.trim().length() == 0;
+    }
+}
